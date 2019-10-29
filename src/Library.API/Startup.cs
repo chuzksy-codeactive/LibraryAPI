42c2c10accf4d8d56b12f32e3da290a72653b0aa
onuchukwuchika <chuzksy@gmail.com>
2019-10-29 22:49:05 +0100

    - configure accept header to support json and xml - configure to return 406 for unsupported header types

--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -9,6 +9,7 @@ using Library.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,7 +30,11 @@ namespace Library.API
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices (IServiceCollection services)
         {
-            services.AddMvc ();
+            services.AddMvc (setupAction => 
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+                setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            });
 
             // register the DbContext on the container, getting the connection string from
             // appSettings (note: use this during development; in a production environment,
