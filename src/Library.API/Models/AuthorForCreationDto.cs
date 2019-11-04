f135acd3397dd5ab3a9355e1083b36d91944c8d7
onuchukwuchika <chuzksy@gmail.com>
2019-11-04 22:25:14 +0100

    - Add an array model binder - Create endpoint to get collections of authors - Create endpoint to add collections of authors

--- src/Library.API/Models/AuthorForCreationDto.cs
+++ src/Library.API/Models/AuthorForCreationDto.cs
@@ -1,12 +1,35 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Library.API.Models
 {
     public class AuthorForCreationDto
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTimeOffset DateOfBirth { get; set; }
-        public string Genre { get; set; }
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public DateTimeOffset DateOfBirth
+        {
+            get;
+            set;
+        }
+        public string Genre
+        {
+            get;
+            set;
+        }
+        public ICollection<BookForCreationDto> AuthorCollection
+        {
+            get;
+            set;
+        } = new List<BookForCreationDto> ();
     }
 }
